9c91a5b49c5785ff0a8bcd3c96f5e1df595f5865
Fábio Nogueira de Lucena <fabio@engenhariadesoftware.inf.br>
2016-10-30 10:04:22 -0200

    refatoracao

---
+++ src/test/java/com/github/kyriosdata/pack/PackExceptionTest.java
@@ -0,0 +1,12 @@
+package com.github.kyriosdata.pack;
+
+import org.junit.jupiter.api.Test;
+
+public class PackExceptionTest {
+
+    @Test
+    public void apenasCriaInstancia() {
+        new PackException(new RuntimeException("a").getMessage());
+    }
+}
+
